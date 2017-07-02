bd3b806084d6e3789293d0b2f121a8a6e5cecba2
bipin patel <bpnptl24@gmail.com>
2017-07-02 12:40:39 +0530

    Active class add sidebar

--- resources/views/includes/sidebar.blade.php
+++ resources/views/includes/sidebar.blade.php
@@ -1,17 +1,17 @@
 <aside class="main-sidebar">
     <section class="sidebar">
         <ul class="sidebar-menu">
-            <li class="active treeview">
+            <li class="@if (Request::is('/')) {{'active'}} @endif treeview">
                 <a href="{{ url('/') }}">
                     <i class="fa fa-dashboard"></i> <span>Dashboard</span>
                 </a>
             </li>
-            <li class="treeview">
+            <li class="@if (Request::is('user')) {{'active'}} @endif treeview">
                 <a href="{{ url('user') }}">
                     <i class="fa fa-user"></i> <span>User</span>
                 </a>
             </li>
-            <li class="treeview">
+            <li class="@if (Request::is('account')) {{'active'}} @endif treeview">
                 <a href="{{ url('account') }}">
                     <i class="fa fa-university"></i> <span>Account</span>
                 </a>
